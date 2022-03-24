3c8596c960f2874047510bdda37b4c8e97b1086e
razzaksr <razzaksr@gmail.com>
2022-03-24 10:48:46 +0530

    edit delete

--- DynamicWeb/crudl/urls.py
+++ DynamicWeb/crudl/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('new',views.hai),
     path('',views.showing),
-    path('<int:unique>',views.reading)
+    path('<int:unique>',views.reading),
+    path('new/<int:key>',views.hai),
+    path('del/<int:pos>',views.remove)
 ]
--- DynamicWeb/crudl/views.py
+++ DynamicWeb/crudl/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import request
 
 from . import models
@@ -8,18 +8,27 @@ from . import forms
 
 house=[]
 
-def hai(request):
+def hai(request,key=0):#new/update
+    #post section
     if request.method=="POST":
-        obj=forms.CorporatesForm(request.POST)
+        if key!=0: #update changes ia POST
+            each=models.Corporates.objects.get(id=key)
+            obj=forms.CorporatesForm(request.POST,instance=each)
+        else: #new addition to corporates
+            obj=forms.CorporatesForm(request.POST)
         if obj.is_valid():
-            obj.save()# insertion
-            obj=forms.CorporatesForm()
-            return render(request,'fill.html',{"submission":obj,"data":"Corporate inserted"})
-        else:
-            return render(request,'fill.html',{"submission":obj,"data":"Corporate hasn't inserted"})
+            obj.save()# insertion/ update
+            #obj=forms.CorporatesForm()
+            #return render(request,'fill.html',{"submission":obj,"data":"Corporate inserted"})
+            return redirect("/crudl/")
+    #get section
     else:
-        obj=forms.CorporatesForm()
-    return render(request,'fill.html',{"submission":obj})
+        if key!=0: #get corporate to edit
+            each=models.Corporates.objects.get(id=key)
+            obj=forms.CorporatesForm(instance=each)
+        else: # show empty form to add new one
+            obj=forms.CorporatesForm()
+        return render(request,'fill.html',{"submission":obj})
 
 def showing(request):
     house=models.Corporates.objects.all();
@@ -28,3 +37,14 @@ def showing(request):
 def reading(request,unique):
     each=models.Corporates.objects.get(id=unique)
     return render(request,'read.html',{"one":each})
+
+def editing(request,key):
+    each=models.Corporates.objects.get(id=key)
+    forming=forms.CorporatesForm(request.POST,instance=each)
+    #print(forming.org)
+    return render(request,'fill.html',{"submission":forming})
+
+def remove(request,pos):
+    each=models.Corporates.objects.get(id=pos)
+    models.Corporates.delete(each)
+    return redirect("/crudl/")
